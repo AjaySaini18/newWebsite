1b451b8aa0391abb75a17a35a4caafd3530c4b68
karansoni7973 <karan.soni@synopix.com>
2025-09-12 18:24:31 +0530

    +1

--- src/components/AIHeroSection.jsx
+++ src/components/AIHeroSection.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { motion } from "framer-motion";
-import { useScroll } from "../Context/ScrollContext";
+import { useScroll } from "../context/ScrollContext";
 
 const AIHeroSection = ({
   title,
